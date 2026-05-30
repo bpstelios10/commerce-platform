b1d7404d2c21ca2870b97e77d4583ee94e5b98c0
Stelios Anastasakis <bpstelios10@hotmail.com>
2026-05-30 15:11:51 +0300

    [products] Added first In-Memory repo

--- services/products/cmd/main.go
+++ services/products/cmd/main.go
@@ -5,6 +5,7 @@ import (
 
 	handler "commerce-platform/services/products/internal/http"
 	"commerce-platform/services/products/internal/product"
+	"commerce-platform/services/products/internal/repository"
 
 	"fmt"
 
@@ -29,6 +30,28 @@ func main() {
 	fmt.Println(product1.Price)
 
 	fmt.Println(product1.IsExpensive())
+
+	fmt.Println("--- TESTING CODE ---")
+	products := map[string]product.Product{
+		"1": {
+			ID:    "1",
+			Name:  "MacBook Pro",
+			Price: 2500,
+		},
+	}
+
+	p, found := products["1"]
+	fmt.Println(p)
+	fmt.Println(found)
+
+	p2, found2 := products["999"]
+	fmt.Println(p2)
+	fmt.Println(found2)
+
+	var repo repository.ProductRepository
+	repo = repository.NewInMemoryProductRepository()
+
+	fmt.Println(repo.FindAll())
 
 	fmt.Println("--- REAL LOGIC ---")
 
---
+++ services/products/internal/repository/product_repository.go
@@ -0,0 +1,44 @@
+package repository
+
+import "commerce-platform/services/products/internal/product"
+
+type ProductRepository interface {
+	FindByID(id string) (product.Product, bool)
+	FindAll() []product.Product
+}
+
+type InMemoryProductRepository struct {
+	products map[string]product.Product
+}
+
+func NewInMemoryProductRepository() *InMemoryProductRepository {
+	return &InMemoryProductRepository{
+		products: map[string]product.Product{
+			"1": {
+				ID:    "1",
+				Name:  "MacBook Pro",
+				Price: 2500,
+			},
+			"2": {
+				ID:    "2",
+				Name:  "iPhone",
+				Price: 1200,
+			},
+		},
+	}
+}
+
+func (r *InMemoryProductRepository) FindAll() []product.Product {
+	var products []product.Product
+
+	for _, p := range r.products {
+		products = append(products, p)
+	}
+
+	return products
+}
+
+func (r *InMemoryProductRepository) FindByID(id string) (product.Product, bool) {
+	p, found := r.products[id]
+	return p, found
+}
