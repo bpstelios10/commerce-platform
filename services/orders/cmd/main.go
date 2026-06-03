1d7eecbef71c093e96b29235cb9a729ea2e436a3
Stelios Anastasakis <bpstelios10@hotmail.com>
2026-06-03 19:23:40 +0300

    [orders] Start orders service

--- services/orders/cmd/main.go
+++ services/orders/cmd/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	httpx "commerce-platform/services/orders/internal/http"
+
+	"github.com/go-chi/chi/v5"
+)
 
 func main() {
-	fmt.Println("Commerce Platform")
+	fmt.Println("Commerce Platform - ORDERS")
+
+	healthHandler := httpx.NewHealthHandler()
+
+	r := chi.NewRouter()
+
+	healthHandler.RegisterRoutes(r)
+
+	http.ListenAndServe(":8080", r)
 }
