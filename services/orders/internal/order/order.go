3a2210207a3660449db29bb4e28c94ea3de57a6e
Stelios Anastasakis <bpstelios10@hotmail.com>
2026-06-08 22:51:37 +0300

    [orders] Added repository

---
+++ services/orders/internal/order/order.go
@@ -0,0 +1,17 @@
+package order
+
+type OrderStatus string
+
+const (
+	CREATED   OrderStatus = "CREATED"
+	PAID      OrderStatus = "PAID"
+	RETURNED  OrderStatus = "RETURNED"
+	CANCELLED OrderStatus = "CANCELLED"
+)
+
+type Order struct {
+	ID        string      `json:"id"`
+	ProductID string      `json:"product_id"`
+	Quantity  int         `json:"quantity"`
+	Status    OrderStatus `json:"status"`
+}
---
+++ services/orders/internal/repository/order_repository.go
@@ -0,0 +1,42 @@
+package repository
+
+import "commerce-platform/services/orders/internal/order"
+
+type InMemoryOrderRepository struct {
+	orders map[string]order.Order
+}
+
+func NewInMemoryOrderRepository() *InMemoryOrderRepository {
+	return &InMemoryOrderRepository{
+		orders: map[string]order.Order{
+			"1": {
+				ID:        "1",
+				ProductID: "1",
+				Quantity:  2,
+				Status:    order.CREATED,
+			},
+			"2": {
+				ID:        "2",
+				ProductID: "2",
+				Quantity:  1,
+				Status:    order.PAID,
+			},
+		},
+	}
+}
+
+func (repo *InMemoryOrderRepository) FindAll() []order.Order {
+	var orders []order.Order
+
+	for _, o := range repo.orders {
+		orders = append(orders, o)
+	}
+
+	return orders
+}
+
+func (repo *InMemoryOrderRepository) FindByID(id string) (order.Order, bool) {
+	o, found := repo.orders[id]
+
+	return o, found
+}
