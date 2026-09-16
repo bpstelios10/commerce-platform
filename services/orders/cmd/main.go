90cc7b58ed4aaa7d9168e5c100d8fec7baa49c8c
Stelios Anastasakis <bpstelios10@hotmail.com>
2026-09-17 00:02:05 +0300

    [orders] Added migration script for orders

--- services/orders/cmd/main.go
+++ services/orders/cmd/main.go
@@ -14,6 +14,7 @@ import (
 	httpx "commerce-platform/services/orders/internal/http"
 	"commerce-platform/services/orders/internal/repository"
 	"commerce-platform/services/orders/internal/service"
+	"commerce-platform/services/orders/migrations"
 	loggerx "commerce-platform/shared/logger"
 	shutdownx "commerce-platform/shared/shutdown"
 
@@ -46,6 +47,12 @@ func main() {
 	}
 	if err := db.Ping(ctx); err != nil {
 		logger.Fatal().Err(err).Msg("failed to connect to postgres")
+	}
+
+	// ---- Migrations ----
+	err = database.RunMigrations(cfg.Database, migrations.Files)
+	if err != nil {
+		logger.Fatal().Err(err).Msg("failed to run database migrations")
 	}
 	defer db.Close()
 
---
+++ services/orders/migrations/001_create_orders.up.sql
@@ -0,0 +1,9 @@
+-- 001_create_orders.up.sql
+
+CREATE TABLE orders (
+    id UUID PRIMARY KEY,
+    product_id VARCHAR(50) NOT NULL,
+    quantity INT NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
